d460a24bff3dab46b33aec6fdc3bbf66cb61acdd
johnnylin9708 <johnnyjclin@gmail.com>
2024-07-24 00:31:08 +0800

    url modified

--- src/API/index.ts
+++ src/API/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { AuthInfo, InvitationInfo, MessageInfo } from "types/type";
 
-const GATEWAY_URL = process.env.BASE_URL;
+const GATEWAY_URL = `${process.env.BASE_URL}/api`;
 // const GATEWAY_URL = "http://localhost:8080/api";
 
 export const login = async (loginInfo: AuthInfo) =>
